47964cec055ff97aef373fd81cb0c9afca0ec9aa
Gustavo <guestudos02@gmail.com>
2025-06-02 09:33:54 -0300

    arrumando o codigo

--- src/js/background.js
+++ src/js/background.js
@@ -2,13 +2,11 @@ const colorSelector = document.getElementById('colorSelector');
 const toggleSlideshow = document.getElementById('toggleSlideshow');
 const banner = document.getElementById('banner');
 
-// ---------- Função para aplicar a cor -----------
 function applyColor(color) {
     document.body.style.backgroundColor = color;
     document.documentElement.style.setProperty('--bg-color', color);
 }
 
-// ---------- Função para mostrar ou ocultar slideshow ----------
 function updateSlideshowVisibility() {
     if (toggleSlideshow.checked) {
         banner.classList.remove('hidden');
@@ -17,7 +15,6 @@ function updateSlideshowVisibility() {
     }
 }
 
-// ---------- Eventos ----------
 colorSelector.addEventListener('change', function () {
     const selectedColor = this.value;
     applyColor(selectedColor);
@@ -30,7 +27,6 @@ toggleSlideshow.addEventListener('change', function () {
     localStorage.setItem('showSlideshow', this.checked);
 });
 
-// ---------- Carregar Configurações ao Iniciar ----------
 window.addEventListener('load', function () {
     const savedColor = localStorage.getItem('bgColor');
     const savedSlideshow = localStorage.getItem('showSlideshow');
